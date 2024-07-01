770eb10a2fef1c16f5afc3c25e376a340cb605e3
Mahendra Singamsetty <mahendrasv@gmail.com>
2024-07-01 08:52:47 +0530

    mobile css updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,78 +1,16 @@
-import { buttonVariants } from "@/components/ui/button";
-import { siteConfig } from "@/config/site";
-import { cn, mobileCheck, sortPosts } from "@/lib/utils";
-import { posts } from "#site/content";
-import Link from "next/link";
-import { PostItem } from "@/components/post-item";
-import { headers } from "next/headers";
 import MobileHome from "@/components/mobile-home";
 import Posts from "@/components/posts";
 
 export default function Home() {
-	const latestPosts = sortPosts(posts).slice(0, 5);
-	const userAgent = headers().get("user-agent") || "";
-	const isMobile = mobileCheck(userAgent);
 
 	return (
-		<div className={!isMobile ? "container flex lg:p-0 lg:gap-2" : ""}>
-			<div className={cn("flex-1", !isMobile ? "sblog" : "")}>
-				<MobileHome />
+		<div className="container max-w-screen-2xl flex lg:p-0 lg:gap-2 pr-0 pl-0 lg:pl-2 lg:pr-2">
+			<div className="flex-1 lg:border-r lg:border-[#009D2C]">
+				<MobileHome/>
 			</div>
-			<div className="hidden md:inline-flex" style={{ flex: 3 }}>
-				<Posts showTags={false} />
+			<div className="hidden md:inline-flex" style={{flex: 3}}>
+				<Posts showTags={false}/>
 			</div>
 		</div>
 	);
-
-	// return isMobile ? <MobileHome /> : (
-	//   <>
-	//     <section className="space-y-6 pb-8 pt-6 md:ph-12 md:mt-10 lg:py-32">
-	//       <div className="container flex flex-col gap-4 text-center">
-	//         <h1 className="tet-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
-	//           Hello, I&apos;m Mahendra Singamsetty
-	//         </h1>
-	//         <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
-	//           Welcome to my blog template. Build using tailwind, shadcn, velite
-	//           and nextjs 14
-	//         </p>
-	//         <div className="flex flex-col gap-4 justify-center sm:flex-row">
-	//           <Link
-	//             href="/blog"
-	//             className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
-	//           >
-	//             View my blog
-	//           </Link>
-	//           <Link
-	//             href={siteConfig.links.github}
-	//             target="_blank"
-	//             rel="noreferrer"
-	//             className={cn(
-	//               buttonVariants({ variant: "outline", size: "lg" }),
-	//               "w-full sm:w-fit"
-	//             )}
-	//           >
-	//             GitHub
-	//           </Link>
-	//         </div>
-	//       </div>
-	//     </section>
-	//     <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60">
-	//       <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
-	//         Latest Posts
-	//       </h2>
-	//       <ul className="flex flex-col">
-	//         {latestPosts.map((post) => (
-	//           <li key={post.slug} className="first:border-t first:border-border">
-	//             <PostItem
-	//               slug={post.slug}
-	//               title={post.title}
-	//               description={post.description}
-	//               date={post.date}
-	//             />
-	//           </li>
-	//         ))}
-	//       </ul>
-	//     </section>
-	//   </>
-	// );
 }
